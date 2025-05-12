07ffa5cb46980461fb064d7cab3e332c028c0ab4
Satoko Nakajima <20104137@tafe.wa.edu.au>
2025-05-12 16:22:49 +0800

    change navigation menu icon and dropdown & style.css

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 
-        <!-- <div class="frontTop" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/map.jpg');">
+<!-- <div class="frontTop" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/map.jpg');">
             <h1>Welcome to TAFE Library!</h1>
         </div> -->
 
@@ -9,20 +9,21 @@
 <div class="map-container">
     <h1 id="welcome-title">Welcome to TAFE Library!</h1>
 
-    <iframe href="https://www.mappedin.com/" 
-            title="Mappedin Map" 
-            name="Mappedin Map" 
-            allow="clipboard-write 'self' https://app.mappedin.com; web-share 'self' https://app.mappedin.com" 
-            scrolling="no" 
-            width="100%" 
-            height="1920" 
-            frameborder="0" 
-            style="border:0" 
-            src="https://app.mappedin.com/map/6705e568cda55a000ba372bc?embedded=true">
-        </iframe>
+    <iframe href="https://www.mappedin.com/"
+        title="Mappedin Map"
+        name="Mappedin Map"
+        allow="clipboard-write 'self' https://app.mappedin.com; web-share 'self' https://app.mappedin.com"
+        scrolling="no"
+        width="100%"
+        height="1920"
+        frameborder="0"
+        style="border:0"
+        src="https://app.mappedin.com/map/6705e568cda55a000ba372bc?embedded=true">
+    </iframe>
 </div>
 
-    </div>
+</div>
 
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -1,41 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<script src="https://cdn.userway.org/widget.js" data-account="FYgjF0BqUB"></script>
-<link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible&display=swap" rel="stylesheet">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php
-        if ( is_front_page() ) {
-            echo 'Wayfinder';
-        } elseif (is_single() || is_page() ){
-            echo get_the_title();
-        } else{
-            bloginfo('name');
-        }
-        ?>
-</title>
-<?php wp_head(); ?>
+    <script src="https://cdn.userway.org/widget.js" data-account="FYgjF0BqUB"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php
+            if (is_front_page()) {
+                echo 'Wayfinder';
+            } elseif (is_single() || is_page()) {
+                echo get_the_title();
+            } else {
+                bloginfo('name');
+            }
+            ?>
+    </title>
+    <?php wp_head(); ?>
 </head>
+
 <body>
-    <div id = wrapper>
-    
+    <div id="wrapper">
         <header class="menu-wrap">
-            <input type="checkbox" id="checkbox"> <!-- Checkbox allows for opening and closing of the hamburger menu -->
-            <label for="checkbox" class="menu-icon">☰</label>
 
-            <label for="checkbox" class="title"><a href="<?php echo home_url(); ?>">
-                <span>
-                    <!-- <img src="<?php echo get_template_directory_uri(); ?>/logo.png" alt="Logo" class="logo"> -->
-                </span>
-                Way Finder</a></label>
+            <?php if (is_front_page()) : ?>
+                <!-- hunbarger icon -->
+                <div class="menu-icon" id="menu-toggle">☰</div>
+            <?php else : ?>
+                <!-- else go back with ← -->
+                <div class="menu-icon back-icon" onclick="history.back();">←</div>
+            <?php endif; ?>
 
-            <div class="custom-menu-class">
-                <?php 
-                    wp_nav_menu( array( 
-                    'theme_location' => 'my-custom-menu', 
-                    'container_class' => 'custom-menu-class' ) ); 
-                ?>
+            <!-- Wayfinder in center -->
+            <div class="title">
+                <a href="<?php echo home_url(); ?>">Wayfinder</a>
             </div>
+
+            <!-- navigation menu -->
+            <nav class="custom-menu-class" id="main-menu">
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'my-custom-menu',
+                    'container' => false
+                ));
+                ?>
+            </nav>
         </header>
-
-
