b8738adb0cb54a191f7faaea65702055a088aaa0
YuiMigaki <yuimigaki1221@icloud.com>
2025-05-13 11:25:17 +0800

    Updated css and cotact-page

--- page-contact.php
+++ page-contact.php
@@ -5,13 +5,15 @@
             <h1>Contact Us</h1>
             <h2>
                 <ul id=contact-list>
-                <li><img src="<?= get_template_directory_uri(); ?>/img/icon/icon_mail.png" alt="icon of envelope"> library@nmtafe.wa.edu.au</li>
-                <li><img src="<?= get_template_directory_uri(); ?>/img/icon/icon_phone.png" alt="icon of phone"> 08 9427 1450</li>
-                <li><img src="<?= get_template_directory_uri(); ?>/img/icon/icon_map.png" alt="icon of map pointer"> 30 Aberdeen Street, Perth 6000</li>
-                <li><img src="<?= get_template_directory_uri(); ?>/img/icon/icon_clock.png" alt="icon of clock"> Mon - Thu: 8:00am - 6:00pm</li>
-                <li><pre>     Fri: 8:00am - 5:00pm</pre></li>
-                <li><pre>     (During Semester)</pre></li>
-                <li><pre>     Closed on Public Holidays</pre></li>
+                    <div id="contact-info-container">
+                        <li class="contact-info"><img src="<?= get_template_directory_uri(); ?>/img/icon/icon_mail.png" alt="icon of envelope"> library@nmtafe.wa.edu.au</li>
+                        <li class="contact-info"><img src="<?= get_template_directory_uri(); ?>/img/icon/icon_phone.png" alt="icon of phone"> 08 9427 1450</li>
+                        <li class="contact-info"><img src="<?= get_template_directory_uri(); ?>/img/icon/icon_map.png" alt="icon of map pointer"> 30 Aberdeen Street, Perth 6000</li>
+                        <li class="contact-info"><img src="<?= get_template_directory_uri(); ?>/img/icon/icon_clock.png" alt="icon of clock"> Mon - Thu: 8:00am - 6:00pm</li>
+                        <li class="contact-info"><pre>     Fri: 8:00am - 5:00pm</pre></li>
+                        <li class="contact-info"><pre>     (During Semester)</pre></li>
+                        <li class="contact-info"><pre>     Closed on Public Holidays</pre></li>
+                    </div>
                 <li id="contact-text">For term/semester break hours, please visit our library website.</li>
                 <li id="contact-library-link"><a href="https://guides.dtwd.wa.gov.au/nmtafe-library">TAFE Library Website</a></li>
                 </ul>
