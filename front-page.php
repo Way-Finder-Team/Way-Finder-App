03a0a9c520521299b8fd2e1774a22284bd08be5b
Yang Wang <20112222@tafe.wa.edu.au>
2025-05-22 11:41:47 +0800

    fixed HTML errors

--- front-page.php
+++ front-page.php
@@ -9,15 +9,10 @@
 <div class="map-container">
     <h1 id="welcome-title">Welcome to TAFE Library!</h1>
 
-    <iframe href="https://www.mappedin.com/"
+    <iframe 
         title="Mappedin Map"
         name="Mappedin Map"
         allow="clipboard-write 'self' https://app.mappedin.com; web-share 'self' https://app.mappedin.com"
-        scrolling="no"
-        width="100%"
-        top="var(--menu-height)"
-        height="calc(100vh - var(--menu-height))"
-        frameborder="0"
         style="border:0"
         src="https://app.mappedin.com/map/6705e568cda55a000ba372bc?embedded=true">
     </iframe>
