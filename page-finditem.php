2d2b05d08deefd8057d732dc55a2a6eae1f97503
YuiMigaki <yuimigaki1221@icloud.com>
2025-05-29 19:34:46 +0800

    fixed:spelling mistakes and added new screenshot

--- page-finditem.php
+++ page-finditem.php
@@ -4,8 +4,8 @@
             <img class="background-vector" src="<?php echo get_template_directory_uri(); ?>/img/category-background.png" alt="classroom 260. Desks and chairs with computers, whiteboards and projector.">
             <h1>Find Library Items</h1>
             <p>Students can browse the Library Catalogue with the computer opposite the Service Desk. 
-            This tool enables the user to serach by title, author, subject or keywords 
-            which can befurther refined with advanced filters to check an item's location and availability.</p>
+            This tool enables the user to search by title, author, subject or keywords
+            which can be further refined with advanced filters to check an item's location and availability.</p>
             <h2>Note: This Catalogue can be accessed from the North Metropolitan TAFE Library website at any time.</h2>
             <div class="image-container">
                 <img src="<?= get_template_directory_uri(); ?>/img/findItem.png" alt="computer on a table with sign of Library Catalogue." class="category-images">
